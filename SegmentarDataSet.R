b4bccbba18edd1fd2c4308122e85198e0816b2e0
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-02-10 20:14:19 -0600

    Create SegmentarDataSet.R

---
+++ SegmentarDataSet.R
@@ -0,0 +1,16 @@
+df <- iris
+set.seed(1234)
+
+# OPCION 1
+ind <- sample(2, nrow(df), replace = TRUE, prob = c(0.7, 0.3))
+random.train <- df[ind == 1,]
+random.test <- df[ind == 2,]
+
+# OPCION 2
+trainTestSplit <- floor(nrow(df)*0.7)
+
+# Asignacion de los datos de entrenamiento
+train <- df[1:trainTestSplit,]
+
+# Datos de prueba
+test <- df[1:trainTestSplit,]
