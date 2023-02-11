309586a75c8a2fc359c608a7f33c51249ee205de
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-02-10 19:44:03 -0600

    Update Norm&Stand.R

--- Norm&Stand.R
+++ Norm&Stand.R
@@ -65,3 +65,49 @@ write.table(crxEstandar, file = "C:/Users/ISND89/Documents/estandar?.txt", sep =
 summary(crx)
 summary(crxNormal)
 summary(crxEstandar)
+
+
+
+
+# =-=-=-=-= OTRO MODO =-=-=-=-=
+crx <- read.csv("C:/Users/ISND89/Downloads/crx.data", header=FALSE, na.strings="?")
+
+colNumericas<-c(2, 3, 8, 11, 14, 15)
+for (i in colNumericas) {
+  posV<-which(is.na(crx[,i]))
+  mediaV<-mean(!is.na(crx[,i]))
+  crx[posV,i] <- mediaV
+}
+summary(crx)
+
+#función para normalizar
+min_max_norm <- function(x) {
+  (x - min(x)) / (max(x) - min(x))
+}
+
+crxNormal <- crx
+#apply Min-Max normalization 
+crxNormal[,colNumericas] <- as.data.frame(lapply(crxNormal[,colNumericas], min_max_norm))
+
+# Vaciar los datos a un archivo
+write.table(crxNormal, file = "C:/Users/ISND89/Documents/normalizarNA.txt", sep = "\t", eol= "\n", dec = ".")
+
+standarizacion<-function(x) {
+  (x - mean(x))/sd(x)
+}
+
+crxEstandar<-crx
+#estandardiza todas las columnas numéricas
+crxEstandar[,colNumericas] <- as.data.frame(lapply(crxEstandar[,colNumericas], standarizacion))
+
+#estandardiza solo una columna
+#crx[,2] <- (crx[,2] - mean(crx[,2])) / sd(crx[,2])
+
+# Vaciar los datos a un archivo
+write.table(crxEstandar, file = "C:/Users/ISND89/Documents/estandarNA.txt", sep = "\t", eol= "\n", dec = ".")
+
+summary(crx)
+summary(crxNormal)
+summary(crxEstandar)
+
+
