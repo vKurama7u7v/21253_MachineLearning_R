0ea08ab37c8b5a815cd4c1197a443b4aeb0922fe
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-02-19 22:46:36 -0600

    Update KNN.R

--- KNN.R
+++ KNN.R
@@ -1,18 +1,49 @@
+#
 df <- iris
 set.seed(1234)
 
 # OPCION 1
 ind <- sample(2, nrow(df), replace = TRUE, prob = c(0.7, 0.3))
+
+# Segmantación aleatoria del conjunto de datos
 random.train <- df[ind == 1,]
 random.test <- df[ind == 2,]
 
-instTrain <- random.train[1, 1:4]
-instTest <- random.test[1, 1:4]
-
-# FUNCION
+# Función para calcular la distancia
 distanciaManhattan <- function(x, y) {
   result <- sum(abs(x-y))
 }
 
-dist <- distanciaManhattan(instTrain, instTest)
-dist
+# Inicializando vector dist (todas las columnas)
+dist <- c()
+
+# Ciclo for de 1 a 38
+for (i in 1:38) {
+  
+  # Inicializando vector lista (cada columna)
+  lista <- c()
+  
+  # Ciclo for de 1 a 112
+  for (j in 1:112) {
+    # Se asignan las instancias(primera) de cada 
+    # subconjunto de datos(train y test)
+    instTrain <- random.train[j, 1:4]
+    instTest <- random.test[i, 1:4]
+    
+    # Se calcula la distancia que existe entre el train y test
+    result <- distanciaManhattan(instTrain, instTest)
+    
+    # Se almacena en resultado a la lista
+    lista <- c(lista, round(result,2))
+  }
+  
+  # Se agrega la columna de resultados (list)
+  dist <- cbind(dist, lista)
+}
+
+# Generando un data.frame
+distancias <- data.frame(dist)
+
+# Generando txt con las distancias
+write.table(distancias, file = "/Volumes/Macintosh Expansion - Data/Macintosh Expansión/distancias.txt", sep = "\t", eol= "\n", dec = ".")
+
