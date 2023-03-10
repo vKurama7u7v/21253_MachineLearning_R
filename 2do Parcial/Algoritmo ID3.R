6ae6ef0e057aed5b12f8701b94b9b956f7c84809
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-03-10 16:39:03 -0600

    Update Algoritmo ID3.R

--- 2do Parcial/Algoritmo ID3.R
+++ 2do Parcial/Algoritmo ID3.R
@@ -1,21 +1,30 @@
-# Paquete de datos
+# Instalando librerias
 install.packages('titanic')
 install.packages('rpart')
 install.packages('rattle')
 install.packages('rpart.plot')
+install.packages("readxl");
 
 # Cargar librarias de analisis
 library(tidyverse)
 
 # Cargar librerias de datos
 library(titanic)
-data("titanic_train")
-head(titanic_train)
+# data("titanic_train")
+# head(titanic_train)
 
 # Cargar librerias para clasificacion
 library(rpart)
 library(rattle)
 library(rpart.plot)
+
+# Mandando a llamar las librerías
+library(readxl)     # Leer archivo de excel 
+
+# Importamos archivo Excel
+data <- read_excel(file.choose(), sheet = 1)
+as.data.frame(data)
+
 
 # Modelando con arboles de decision
 arbol <- rpart(
