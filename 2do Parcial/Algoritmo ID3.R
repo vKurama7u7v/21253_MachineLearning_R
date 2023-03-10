de04347af2442a766923912f6610a08c960694cb
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-03-10 15:16:34 -0600

    add ID3

---
+++ 2do Parcial/Algoritmo ID3.R
@@ -0,0 +1,28 @@
+# Paquete de datos
+install.packages('titanic')
+install.packages('rpart')
+install.packages('rattle')
+install.packages('rpart.plot')
+
+# Cargar librarias de analisis
+library(tidyverse)
+
+# Cargar librerias de datos
+library(titanic)
+data("titanic_train")
+head(titanic_train)
+
+# Cargar librerias para clasificacion
+library(rpart)
+library(rattle)
+library(rpart.plot)
+
+# Modelando con arboles de decision
+arbol <- rpart(
+  formula = Survived ~ Sex + Age,
+  data = titanic_train,
+  method = 'class'
+)
+
+# Graficando el arbol
+fancyRpartPlot(arbol)
