c0907b79dbd16920dcbeccbbc9074c9aadb6bd0f
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2023-03-29 20:27:48 -0600

    Create ANN Boston.R

---
+++ 2do Parcial/ANN Boston.R
@@ -0,0 +1,43 @@
+
+# Import Required packages
+set.seed(500)
+library(neuralnet)
+library(MASS)
+
+# Boston dataset from MASS
+data <- Boston
+# Normalize the data
+maxs <- apply(data, 2, max)
+mins <- apply(data, 2, min)
+scaled <- as.data.frame(scale(data, center = mins,
+                              scale = maxs - mins))
+
+# Split the data into training and testing set
+index <- sample(1:nrow(data), round(0.75 * nrow(data)))
+train_ <- scaled[index,]
+test_ <- scaled[-index,]
+
+# Build Neural Network
+nn <- neuralnet(medv ~ crim + zn + indus + chas + nox
+                + rm + age + dis + rad + tax +
+                  ptratio + black + lstat,
+                data = train_, hidden = c(5, 3),
+                linear.output = TRUE)
+
+# Predict on test data
+pr.nn <- compute(nn, test_[,1:13])
+
+# Compute mean squared error
+pr.nn_ <- pr.nn$net.result * (max(data$medv) - min(data$medv)) + min(data$medv)
+test.r <- (test_$medv) * (max(data$medv) - min(data$medv)) + min(data$medv)
+
+MSE.nn <- sum((test.r - pr.nn_)^2) / nrow(test_)
+
+# Plot the neural network
+plot(nn)
+
+# Plot regression line
+plot(test_$medv, pr.nn_, col = "red",
+     main = 'Real vs Predicted')
+abline(0, 1, lwd = 2)
+
